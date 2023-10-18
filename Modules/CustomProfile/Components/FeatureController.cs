106ec6aaf2595560679aff7d5a6efa87bf445aa1
Michael Dowd <michael.dowd@dowdian.com>
2023-10-18 12:58:40 +0300

    Rebuilt the boiler plate with the correct project name

---
+++ Modules/CustomProfile/Components/FeatureController.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Search;
+
+namespace Dowdian.Modules.CustomProfile.Components
+{
+    public class CustomProfileController 
+    {
+
+    }
+}
