ffe2fd009b66244969ce435c080a225dad88e7d8
Michael Dowd <michael.dowd@dowdian.com>
2023-10-18 12:47:33 +0300

    Added project boiler plate

---
+++ Modules/DowdianDnnCustomProfile/Components/FeatureController.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Search;
+
+namespace Dowdian.Modules.DowdianDnnCustomProfile.Components
+{
+    public class DowdianDnnCustomProfileController 
+    {
+
+    }
+}
---
+++ Modules/DowdianDnnCustomProfile/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DowdianDnnCustomProfile Modules Extension for DNN")]
+[assembly: AssemblyDescription("A custom module for displaying and editing the DNN user profile.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dowdian")]
+[assembly: AssemblyProduct("DowdianDnnCustomProfile Modules Extension for DNN")]
+[assembly: AssemblyCopyright("Copyright 2023 Dowdian")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56E36EC0-3EE2-4629-9364-918E9A3DC235")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("01.00.00")]
+[assembly: AssemblyFileVersion("01.00.00")]
